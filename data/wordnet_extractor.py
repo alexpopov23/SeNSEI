f5230d25d96d0d911f4786bd7e897d603d30349c
alexpopov23 <alx.popov23@gmail.com>
2020-06-17 15:37:49 +0300

    stubs for wordnet extractor

---
+++ data/wordnet_extractor.py
@@ -0,0 +1,26 @@
+
+def get_glosses(path):
+    """
+    Extract a dictionary mapping synset/senses to glosses
+    :param path:
+    :return:
+    """
+    return
+
+def get_relations(path):
+    """
+    Extract relational knowledge from WordNet.
+    Dictionary with relation types as keys and values -- dictionaries that map senses/sysets to senses/synsets.
+    :param path:
+    :return:
+    """
+    return
+
+def construct_sentences():
+    """
+    Construct training sentences in the required format:
+    "[HYPONYM] feline mammal usually having thick soft fur and no ability to roar: domestic cats; wildcats
+     [SEP] cat any of various lithe-bodied roundheaded fissiped mammals, many with retractile claws"
+    :return:
+    """
+    return
